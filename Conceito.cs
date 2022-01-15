42a658ccf5f5c6f949323761cd63ffd80c379704
Night <andreguilherme2003@gmail.com>
2022-01-15 14:00:29 -0300

    progama terminado

---
+++ Conceito.cs
@@ -0,0 +1,11 @@
+namespace Praticando
+{
+    public enum Conceito
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Praticando // Note: actual namespace depends on the project name.
 {
@@ -6,12 +7,11 @@ namespace Praticando // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-
-            Aluno[] alunos = new Aluno[5];
-            int indiceAluno = 0;
+            List<Aluno> alunos = new List<Aluno>();
+            //Aluno[] alunos = new Aluno[5];
             string opc = ObterOpcao();
 
-            while (opc != "4")
+            while (opc != "5")
             {
                 switch(opc) 
                 {
@@ -33,32 +33,83 @@ namespace Praticando // Note: actual namespace depends on the project name.
                         throw new ArgumentException("Valor da nota deve ser decimal");
                     }
 
-                        alunos[indiceAluno] = aluno;
-                        indiceAluno++;
-                        
+                        alunos.Add(aluno);              
+
+                    break;
+
+                    case "2":
+
+                    Console.Write("Nome do aluno a ser excluido: ");
+                    var exAluno = Console.ReadLine();
+
+                        for (int i = 0; i < alunos.Count; i++) 
+                    {
+                            if (alunos[i].Nome == exAluno)
+                        {
+                            alunos.Remove(alunos[i]);
+                        }
+                    }
 
                     break;
 
                     //Listar alunos
-                    case "2":
+                    case "3":
                         foreach (var a in alunos)
                         {
                             if (a.Nome != null)
                             {
                                 Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
                             }
-                            Console.ReadLine();
                         }
+                        Console.WriteLine();
 
 
                     break;
 
-                    case "3":
                     //Media geral dos alunos
+                    case "4":
+                    decimal notaTotal = 0;
+                    var nmAlunos = 0;
+
+                    foreach (Aluno nAluno in alunos) 
+                    {
+                        notaTotal = notaTotal + nAluno.Nota;
+                        nmAlunos++;
+                    }
+
+                    var media = notaTotal / nmAlunos;
+                    Conceito conceitoGeral;
+
+                    if (media <= 2)
+                    {
+                        conceitoGeral = Conceito.E;
+                    }
+                    else if (media <= 4)
+                    {
+                        conceitoGeral = Conceito.D;
+                    }
+                    else if (media <= 6)
+                    {
+                        conceitoGeral = Conceito.C;
+                    }
+                    else if (media <= 8)
+                    {
+                        conceitoGeral = Conceito.B;
+                    }
+                    else
+                    {
+                        conceitoGeral = Conceito.A;
+                    }
+
+                    Console.WriteLine($"Média geral: {media} - Conceito geral: {conceitoGeral}");
+                    Console.WriteLine();
+
                     break;
 
                     default:
-                        throw new ArgumentOutOfRangeException();
+                        Console.WriteLine("Por favor digite um numero de 1 a 5 ");
+                        Console.WriteLine();
+                    break;
                 }
                 opc = ObterOpcao();
             }
@@ -66,12 +117,12 @@ namespace Praticando // Note: actual namespace depends on the project name.
 
         static string ObterOpcao()
         {
-            Console.Clear();
             Console.WriteLine("Selecione uma opção");
             Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Media geral dos alunos");
-            Console.WriteLine("4- Sair");
+            Console.WriteLine("2- Excluir aluno");
+            Console.WriteLine("3- Listar alunos");
+            Console.WriteLine("4- Media geral dos alunos");
+            Console.WriteLine("5- Sair");
             Console.WriteLine();
 
             string opc = Console.ReadLine();
